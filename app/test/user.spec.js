4541c06acb468b0aa0bdefaf2d5960b8d5f5ae65
Ivan Stanevich <i.stanevich@itransition.com>
2019-04-05 10:12:07 +0300

    develop
    update user.spec

--- app/test/user.spec.js
+++ app/test/user.spec.js
@@ -3,8 +3,40 @@ const app = require('../app');
 
 describe('/user', function () {
 
-    const authKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidXNlcjEiLCJpZCI6MTMsInRpbWUiOiIyMDE5LTA0LTA1VDA2OjE5OjI3LjI1MVoiLCJpYXQiOjE1NTQ0NDUxNjcsImV4cCI6MTU1NDU0MTE2N30.Uhj_Bfzg8z-couuMTf3eaabtnQrReqS5Xa2z408j72w';
+    let authKey;
     let userId = 1;
+
+    beforeEach(done => {
+        const userData = {
+            name: 'user',
+            password: 'password'
+        };
+
+        request(app)
+            .post('/user')
+            .send(userData)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+
+                if (res.body.user.id) {
+                    userId = res.body.user.id;
+                }
+
+                request(app)
+                    .post('/authenticate')
+                    .set('Accept', 'application/json')
+                    .send(userData)
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        authKey = res.body.token;
+                        done();
+                    });
+            });
+    });
+
 
     describe('GET', () => {
         it('should respond with json containing a list of all users', done => {
