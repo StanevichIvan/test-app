167a9357d78c4938a20be818e6c5e0328cc2fe0e
Ivan Stanevich <i.stanevich@itransition.com>
2019-04-08 09:25:43 +0300

    develop
    add brackets to the if statements

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -4,12 +4,14 @@ const config =  require('../config');
 const checkAuth = (req, res, next) => {
     const token = req.headers['token'];
 
-    if (!token)
+    if (!token) {
         return res.status(403).send({ auth: false, message: 'No token provided.' });
+    }
 
     jwt.verify(token, config.jwtSecret, (err, decoded) => {
-        if (err)
+        if (err) {
             return res.status(401).send({ auth: false, message: 'Failed to authenticate token.' });
+        }
         req.user = {
             name: decoded.name,
             id: decoded.id
